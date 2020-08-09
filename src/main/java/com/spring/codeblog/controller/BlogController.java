5e0bb1f095d05d935fb7b01beb61eaba2bbc5d12
gugabala <gugabala@gmail.com>
2020-08-09 15:44:47 -0300

    Criando o Formulário para gerar novos Posts

--- src/main/java/com/spring/codeblog/controller/BlogController.java
+++ src/main/java/com/spring/codeblog/controller/BlogController.java
@@ -1,13 +1,18 @@
 package com.spring.codeblog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.BlogService;
@@ -24,12 +29,28 @@ public class BlogController {
 		mv.addObject("posts", posts);
 		return mv;
 	}
-	
-    @RequestMapping(value="/posts/{id}", method=RequestMethod.GET)
-    public ModelAndView getPostDetails(@PathVariable("id") long id){
-        ModelAndView mv = new ModelAndView("postDetails");
-        Post post = blogService.findById(id);
-        mv.addObject("post", post);
-        return mv;
-    }
+
+	@RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+	public ModelAndView getPostDetails(@PathVariable("id") long id) {
+		ModelAndView mv = new ModelAndView("postDetails");
+		Post post = blogService.findById(id);
+		mv.addObject("post", post);
+		return mv;
+	}
+
+	@RequestMapping(value = "/newpost", method = RequestMethod.GET)
+	public String getPostForm() {
+		return "postForm";
+	}
+
+	@RequestMapping(value = "/newpost", method = RequestMethod.POST)
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
+			return "redirect:/newpost";
+		}
+		post.setData(LocalDate.now());
+		blogService.save(post);
+		return "redirect:/posts";
+	}
 }
